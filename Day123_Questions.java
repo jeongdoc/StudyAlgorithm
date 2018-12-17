e8c55122315a40dfae27ed0b7bdacef6811fb25b
DevJE <45855412+DevJE@users.noreply.github.com>
2018-12-17 13:43:59 +0900

    Update Day123_Questions.java
    day1 + day2

--- Day123_Questions.java
+++ Day123_Questions.java
@@ -52,5 +52,60 @@ public class Day123_Questions {//gg
 		return iar;
 
 	}
+	//2018.12.16 Day2
+	public void triangleLB(int n) {
+		for(int i = 0; i < n; i ++) {
+			for(int j = 0; j <= i; j ++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+	public void triangleLU(int n) {
+		for(int i = 0; i < n; i ++) {
+			for(int j = n - 1; j >= i; j --) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+	public void triangleRU(int n) {
+		
+		for(int i = 0; i <= n; i ++) {
+			for(int j = 0; j < i; j ++) {
+				System.out.print("  ");
+			}
+			for(int j = 0; j < n - i; j ++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+	public void triangleRB(int n ) {
+		for(int i = 0; i <= n; i ++) {
+			for(int j = n; i < j; j --) {
+				System.out.print("  ");
+			}
+			for(int j = 0; j < i; j ++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+	public void starSquare() {
+		System.out.println("사각형을 출력합니다.");
+		System.out.print("단수 : ");
+		int n = sc.nextInt();
+		
+		String [][]sar = new String[n][n];
+		
+		for (String[] se : sar) {
+			for(String [] sr : sar) {
+			System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+
 
 }
