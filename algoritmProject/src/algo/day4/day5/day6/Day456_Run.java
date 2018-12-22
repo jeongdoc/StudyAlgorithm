44fea703e68a98cd6c9239bd366a1552c5852af6
DevJE <45855412+DevJE@users.noreply.github.com>
2018-12-23 02:52:59 +0900

    Day456 실행용 클래스

--- algoritmProject/src/algo/day4/day5/day6/Day456_Run.java
+++ algoritmProject/src/algo/day4/day5/day6/Day456_Run.java
@@ -3,16 +3,15 @@ package algo.day4.day5.day6;
 public class Day456_Run {
 
 	public static void main(String[] args) {
-		// Day456_Questions Ãâ·Â¿ë
+		// Day456_Questions ÃƒÃ¢Â·Ã‚Â¿Ã«
 		
-		//Day4 Ãâ·Â
+		//Day4 ÃƒÃ¢Â·Ã‚
 		Day456_Questions boom456 = new Day456_Questions();
 		
 		int []a = boom456.deArray();
 		System.out.println();
 		int p = Day456_Questions.sumOf(a);
-		System.out.println("a[]ÀÇ ÇÕ°è  : " + p);
-		
+		System.out.println("a[]Ã€Ã‡ Ã‡Ã•Â°Ã¨  : " + p);
 	
 
 	}
