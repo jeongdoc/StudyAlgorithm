7f8976df68adffff083162a6a77c2c1933bdfe0e
DevJE <45855412+DevJE@users.noreply.github.com>
2019-04-26 21:10:44 +0900

    Update BOJ15552.java

--- bjALGO/BOJ15552.java
+++ bjALGO/BOJ15552.java
@@ -10,17 +10,17 @@ public class BOJ15552 {
 	public static void main(String[] args) {
 		// boj15552
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        try{
-            int x = Integer.parseInt(br.readLine());
-            for(int i = 0; i < x; i ++) {
-                String []sar = br.readLine().split(" ");
-                bw.write(Integer.parseInt(sar[0]) + Integer.parseInt(sar[1])+ "\n");
-            }
-            bw.flush();
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
+        	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        	try{
+ 	           	int x = Integer.parseInt(br.readLine());
+        	    	for(int i = 0; i < x; i ++) {
+                	String []sar = br.readLine().split(" ");
+                	bw.write(Integer.parseInt(sar[0]) + Integer.parseInt(sar[1])+ "\n");
+            	}
+            	bw.flush();
+        	} catch(Exception e) {
+            		e.printStackTrace();
+	        }
 	}
 
 }
