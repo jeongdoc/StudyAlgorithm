52f763a4b9ce730e0f8b6b811efc09fb889e8b05
macJE <jeong.dev01@gmail.com>
2020-09-20 20:29:07 +0900

    leetcode

--- algoritmProject/src/leetcode/easy/PlusOne.java
+++ algoritmProject/src/leetcode/easy/PlusOne.java
@@ -5,9 +5,20 @@ public class PlusOne {
 	public int[] plusOne(int[] digits) {
 		int[] result = {};
 		
+		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < digits.length; i ++) {
-			System.out.println(digits[i]);
+			sb.append(digits[i]);
 		}
+		String num = sb.toString();
+		int number= Integer.parseInt(num) + 1;
+		System.out.println(number);
+		
+		int newNumber = 0;
+		for(int i = 0; i < num.length(); i ++) {
+			
+			result[i] = 0; 
+		}
+		//System.out.println("re -> " + result[0]);
 		
 		return result;
 	}
