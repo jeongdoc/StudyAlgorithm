8d9fa1b16fdccf5e18579953ab9842214701ecc3
macJE <jeong.dev01@gmail.com>
2020-09-20 20:14:03 +0900

    leetcode

--- algoritmProject/src/leetcode/easy/PlusOne.java
+++ algoritmProject/src/leetcode/easy/PlusOne.java
@@ -5,6 +5,10 @@ public class PlusOne {
 	public int[] plusOne(int[] digits) {
 		int[] result = {};
 		
+		for(int i = 0; i < digits.length; i ++) {
+			System.out.println(digits[i]);
+		}
+		
 		return result;
 	}
 }
