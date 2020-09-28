95e3481a9b80da8aedf183b9214740f8cd879fdf
macJE <jeong.dev01@gmail.com>
2020-09-28 19:47:16 +0900

    leetcode

---
+++ algoritmProject/src/leetcode/easy/ClimbingStairs.java
@@ -0,0 +1,18 @@
+package leetcode.easy;
+
+public class ClimbingStairs {
+	
+	public int climbStairs(int n) {
+		int result = 0;
+		
+		
+		for(int i = 1; i < n; i ++) {
+			
+			
+			
+		}
+		
+		return 0;
+	}
+
+}
