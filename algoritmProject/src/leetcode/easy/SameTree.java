a67861473f35efb5d80d4b1641d19a1a10c4fd58
macJE <jeong.dev01@gmail.com>
2020-10-14 18:08:00 +0900

    leetcode easy 100

--- algoritmProject/src/leetcode/easy/SameTree.java
+++ algoritmProject/src/leetcode/easy/SameTree.java
@@ -14,10 +14,32 @@ public class SameTree {
 	}
 
 	public boolean isSameTree(SameTree p, SameTree q) {
+		// leetCode Easy 100
 		
-		System.out.println(p.left.val);  
-		System.out.println(p.right.val);
+		// recursion
+		// Runtime : 0ms, faster than 100.00% of Java online submissions for Same Tree.
+		boolean flag = false;
+//		
+//		if(p != null && q != null) {
+//			flag = p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+//			
+//			return flag;
+//		} else if(p == null && q == null) {
+//			flag = true;
+//			
+//			return flag;
+//		} else {
+//			return flag;
+//		}
 		
-		return false;
+		// linear
+		// Runtime : 0ms, faster than 100.00% of Java online submissions for Same Tree.
+		if(p == null || q == null) {
+			flag = p == q;
+		} else {
+			flag = p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+		}
+		return flag;
+		
 	}
 }
