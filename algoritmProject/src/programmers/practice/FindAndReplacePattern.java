a0af14e30d82124529d79ba9fe192203a1935271
tansan_JE <45855412+DevJE@users.noreply.github.com>
2021-12-22 08:33:47 +0900

    문풀오류수정

--- algoritmProject/src/programmers/practice/FindAndReplacePattern.java
+++ algoritmProject/src/programmers/practice/FindAndReplacePattern.java
@@ -1,38 +1,37 @@
 import java.util.*;
 class Solution {
     public List<String> findAndReplacePattern(String[] words, String pattern) {
-        char[] getChar = pattern.toCharArray();
         
-        pattern = p(pattern);
+        int[] patternArr = p(pattern);
         
         List<String> find = new ArrayList<>();
+
         for(int i = 0; i < words.length; ++i) {
-            
-            if(pattern.equals(p(words[i]))) {
+            int[] temp = p(words[i]);
+            if(Arrays.equals(patternArr, temp)) {
                 find.add(words[i]);
             }
-            
         }
-
         return find;
     }
     
-    public String p(String word) {
+     public int[] p(String word) {
         
         Map<Character, Integer> findPattern = new HashMap<>();
-        StringBuilder address = new StringBuilder();
+        int[] address = new int[word.length()];
 
         for(int i = 0; i < word.length(); ++i) {
             
+            char temp = word.charAt(i);
             // save location
-            if(!findPattern.containsKey(word.charAt(i))) {
-                findPattern.put(word.charAt(i), findPattern.size());
+            if(!findPattern.containsKey(temp)) {
+                findPattern.put(temp, i);
             }
             
-            address.append(findPattern.get(word.charAt(i)));
+            address[i] = findPattern.get(word.charAt(i));
             
         }
         
-        return address.toString();
+        return address;
     }
 }
