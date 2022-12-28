1f8ee53a7ed858da2e32f1d8b5126b57adacb2ef
jeongdoc <jeong.dev01@gmail.com>
2022-12-28 16:38:25 +0900

    Leetcode easy 169, add 2nd solution

--- algoritmProject/leetcode/easy/MajorityElement.js
+++ algoritmProject/leetcode/easy/MajorityElement.js
@@ -5,16 +5,17 @@
  */
 var majorityElement = function(nums) {
 
+    // O(nlogn)
     if(nums.length < 3) return nums[0];
 
     nums.sort();
     let cnt = 1;
     for(let i = 0; i < nums.length-1; i ++) {
         
-        if((nums[i] ^ nums[i+1]) !== 0) {
-            cnt = 1;
-        } else {
+        if((nums[i] ^ nums[i+1]) === 0) {
             cnt ++;
+        } else {
+            cnt = 1;
         }
 
         if(cnt > Math.floor(nums.length/2)) {
@@ -23,4 +24,27 @@ var majorityElement = function(nums) {
     }
 
 };
-console.log(majorityElement([3,3,4]));
+
+
+var majorityElement2 = function(nums) {
+    if(nums.length < 3) return nums[0];
+
+    let majority = 0;
+    for(let i = 0; i < 32; i ++) {
+        let cnt = 0;
+        for(let j = 0; j < nums.length; j ++) {
+            if((nums[j] >> i) & 1 === 1) {
+                cnt ++;
+            }
+
+            if(cnt > Math.floor(nums.length/2)) {
+                console.log(nums[j])
+                majority |= (1 << i);
+            }
+        }
+    }
+
+    return majority;
+};
+
+console.log("result : ", majorityElement2([2,4,4]));
