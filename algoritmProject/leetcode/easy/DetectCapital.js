28b0ee2c0c71f05adbe6ac8f0e86ba9a258a9e7f
jeongdoc <jeong.dev01@gmail.com>
2023-01-04 09:28:24 +0900

    문제푸는중

---
+++ algoritmProject/leetcode/easy/DetectCapital.js
@@ -0,0 +1,18 @@
+// 520. Detect Capital
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+var detectCapitalUse = function(word) {
+
+    word = "BBAEF";
+
+    
+    
+    for(let i = 0; i < word.length; i ++) {
+
+    }
+
+};
+
+detectCapitalUse("2");
