a3a39335c90eae71e68abb5247a93e1cc5b18e71
jeongdoc <jeong.dev01@gmail.com>
2023-01-30 16:56:28 +0900

    문제푸는중

---
+++ algoritmProject/leetcode/easy/HappyNumber.js
@@ -0,0 +1,23 @@
+// 202. Happy Number
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+
+// Input: n = 19
+// Output: true
+// Explanation:
+// 12 + 92 = 82
+// 82 + 22 = 68
+// 62 + 82 = 100
+// 12 + 02 + 02 = 1
+
+const getsquares = (n) => {
+    
+}
+
+var isHappy = function(n) {
+
+};
+
+isHappy(19);
