4400d5bc59200c8d71a5496b9fafb66a36c4cda4
jeongdoc <jeong.dev01@gmail.com>
2023-02-01 17:38:13 +0900

    문제를 잘못이해했나..

--- algoritmProject/leetcode/easy/HappyNumber.js
+++ algoritmProject/leetcode/easy/HappyNumber.js
@@ -12,12 +12,44 @@
 // 62 + 82 = 100
 // 12 + 02 + 02 = 1
 
-const getsquares = (n) => {
-    
+const getsquares = (n) => {    
+
+    let square = 1;
+    let arr = [];
+    while(n > 0) {
+
+        let pow = Math.pow(10, square);
+        let remain = n % pow;
+        let res = remain / pow * 10;
+        n -= remain;
+
+        let temp = (res ** 2);
+        arr.push(temp);
+
+        square ++;
+    }
+
+    console.log(arr);
+
+    let sum = 0;
+    for(let i = 0; i < arr.length; i ++) {
+        sum += arr[i];
+    }
+
+    console.log(sum);
+
+    if(sum / 10 < 1) {
+        return sum;
+    }
+
+    return getsquares(sum);
 }
 
 var isHappy = function(n) {
+    // console.log("AA : " , getsquares(n));
+    // ?? 문제를 완벽하게 이해 못했나;;
 
+    return getsquares(n) === 1 ? true : false;
 };
 
-isHappy(19);
+console.log(isHappy(1111111));
